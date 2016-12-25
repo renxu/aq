bb13bc37743dbc89ed41ea6a2fd5a2ba790602fe
renxu <renxu@REXU-X1>
2016-12-24 22:05:20 -0800

    Priority queue.

--- AlgorithmQuestions/Queue/PriorityQueue.cs
+++ AlgorithmQuestions/Queue/PriorityQueue.cs
@@ -8,19 +8,59 @@ namespace AlgorithmQuestions
 {
     public class PriorityQueue<T> where T:IComparable
     {
+        private Dictionary<int, Queue<T>> queues = null;
+        private MinHeap<int> priorityHeap = null;
+
+        public PriorityQueue()
+        {
+            queues = new Dictionary<int, Queue<T>>();
+            priorityHeap = new MinHeap<int>(null);
+        }
+
+        /// <summary>
+        /// Time complexity: O(1)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="priority"></param>
         public void Enqueue(T item, int priority)
         {
-            throw new NotImplementedException();
+            if (!this.queues.ContainsKey(priority))
+            {
+                this.queues[priority] = new Queue<T>();
+                priorityHeap.Insert(priority);
+            }
+
+            this.queues[priority].Enqueue(item);
         }
 
+        /// <summary>
+        /// Time complexity: O(logn)
+        /// </summary>
+        /// <returns></returns>
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            int topPriority = priorityHeap.Peak();
+            var queue = this.queues[topPriority];
+            T value = queue.Dequeue();
+
+            if (queue.Count == 0)
+            {
+                queues.Remove(topPriority);
+                priorityHeap.Extract();
+            }
+
+            return value;
         }
 
+        /// <summary>
+        /// Time complexity: O(1)
+        /// </summary>
+        /// <returns></returns>
         public T Peak()
         {
-            throw new NotImplementedException();
+            int topPriority = priorityHeap.Peak();
+            var queue = this.queues[topPriority];
+            return queue.Peek();
         }
     }
 }
--- AlgorithmTests/Queue/PriorityQueueTests.cs
+++ AlgorithmTests/Queue/PriorityQueueTests.cs
@@ -7,7 +7,7 @@ namespace AlgorithmTests
     [TestClass]
     public class PriorityQueueTests
     {
-        //[TestMethod]
+        [TestMethod]
         public void PriorityQueue_EnqueueDequeue_EqualPriority()
         {
             var queue = new PriorityQueue<string>();
@@ -23,6 +23,51 @@ namespace AlgorithmTests
             Assert.AreEqual("D", queue.Dequeue());
             Assert.AreEqual("E", queue.Dequeue());
             Assert.AreEqual("F", queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void PriorityQueue_EnqueueDequeue_UnequalPriority()
+        {
+            var queue = new PriorityQueue<string>();
+            queue.Enqueue("A", 1);
+            queue.Enqueue("B", 2);
+            queue.Enqueue("C", 1);
+            queue.Enqueue("D", 2);
+            queue.Enqueue("E", 1);
+            queue.Enqueue("F", 2);
+            Assert.AreEqual("A", queue.Dequeue());
+            Assert.AreEqual("C", queue.Dequeue());
+            Assert.AreEqual("E", queue.Dequeue());
+            Assert.AreEqual("B", queue.Dequeue());
+            Assert.AreEqual("D", queue.Dequeue());
+            Assert.AreEqual("F", queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void PriorityQueue_EnqueueDequeue_AddRemoveAddSubqueue()
+        {
+            var queue = new PriorityQueue<string>();
+            queue.Enqueue("B", 2);
+            queue.Enqueue("A", 1);
+            Assert.AreEqual("A", queue.Dequeue());
+            queue.Enqueue("D", 2);
+            queue.Enqueue("C", 1);
+            Assert.AreEqual("C", queue.Dequeue());
+            Assert.AreEqual("B", queue.Dequeue());
+            Assert.AreEqual("D", queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void PriorityQueue_Peak_Basic()
+        {
+            var queue = new PriorityQueue<string>();
+            queue.Enqueue("A", 1);
+            queue.Enqueue("B", 2);
+            queue.Enqueue("C", 1);
+            queue.Enqueue("D", 2);
+            Assert.AreEqual("A", queue.Peak());
+            Assert.AreEqual("A", queue.Dequeue());
+            Assert.AreEqual("C", queue.Peak());
         }
     }
 }

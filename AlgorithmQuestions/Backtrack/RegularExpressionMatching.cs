0950fd0de517f10ad498193b0841ed048c936599
renxu <renxu@REXU-X1>
2017-02-07 22:16:41 -0800

    Match regular expression

---
+++ AlgorithmQuestions/Backtrack/RegularExpressionMatching.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions
+{
+    public class RegularExpressionMatching
+    {
+        /*
+        '.' Matches any single character.
+        '*' Matches zero or more of the preceding element.
+
+        The matching should cover the entire input string (not partial).
+
+        The function prototype should be:
+        bool isMatch(const char *s, const char *p)
+
+        Some examples:
+        isMatch("aa","a") → false
+        isMatch("aa","aa") → true
+        isMatch("aaa","aa") → false
+        isMatch("aa", "a*") → true
+        isMatch("aa", ".*") → true
+        isMatch("ab", ".*") → true
+        isMatch("aab", "c*a*b") → true
+        */
+        public bool IsMatch(string s, string p)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("s");
+            }
+
+            if (p == null)
+            {
+                throw new ArgumentNullException("p");
+            }
+
+            if (s.Length == 0 && p.Length == 0)
+            {
+                return true;
+            }
+            else if (s.Length != 0 && p.Length == 0)
+            {
+                return false;
+            }
+
+            return IsMatch(s, 0, p, 0);
+        }
+
+        private bool IsMatch(string input, int inputIndex, string pattern, int patternIndex)
+        {
+            if (inputIndex >= input.Length && patternIndex >= pattern.Length)
+            {
+                return true;
+            }
+            else if (patternIndex >= pattern.Length)
+            {
+                return false;
+            }
+            else if (inputIndex >= input.Length)
+            {
+                while (patternIndex < pattern.Length)
+                {
+                    if (pattern[patternIndex] == '*')
+                    {
+                        throw new ArgumentException("Invalid pattern");
+                    }
+                    
+                    if (patternIndex + 1 < pattern.Length && pattern[patternIndex + 1] == '*')
+                    {
+                        patternIndex = patternIndex + 2;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+
+            if (pattern[patternIndex] == '*')
+            {
+                throw new ArgumentException("Invalid pattern");
+            }
+
+            if (pattern[patternIndex] != '.')
+            {
+                if (patternIndex + 1 >= pattern.Length || pattern[patternIndex + 1] != '*')
+                {
+                    // Single char case
+                    if (input[inputIndex] == pattern[patternIndex])
+                    {
+                        return IsMatch(input, inputIndex + 1, pattern, patternIndex + 1);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    // char* case
+                    if (input[inputIndex] == pattern[patternIndex])
+                    {
+                        return IsMatch(input, inputIndex + 1, pattern, patternIndex) || IsMatch(input, inputIndex, pattern, patternIndex + 2);
+                    }
+                    else
+                    {
+                        return IsMatch(input, inputIndex, pattern, patternIndex + 2);
+                    }
+                }
+            }
+            else
+            {
+                if (patternIndex + 1 >= pattern.Length || pattern[patternIndex + 1] != '*')
+                {
+                    // Single . case
+                    return IsMatch(input, inputIndex + 1, pattern, patternIndex + 1);
+                }
+                else
+                {
+                    // .* case
+                    return IsMatch(input, inputIndex + 1, pattern, patternIndex) || IsMatch(input, inputIndex, pattern, patternIndex + 2);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmTests/Backtrack/RegularExpressionMatchingTests.cs
@@ -0,0 +1,24 @@
+﻿using AlgorithmQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class RegularExpressionMatchingTests
+    { 
+
+        [TestMethod]
+        public void IsMatch_Basics()
+        {
+            RegularExpressionMatching re = new RegularExpressionMatching();
+            Assert.IsFalse(re.IsMatch("aa", "a"));
+            Assert.IsTrue(re.IsMatch("aa", "aa"));
+            Assert.IsFalse(re.IsMatch("aaa", "aa"));
+            Assert.IsTrue(re.IsMatch("aa", "a*"));
+            Assert.IsTrue(re.IsMatch("aa", ".*"));
+            Assert.IsTrue(re.IsMatch("ab", ".*"));
+            Assert.IsTrue(re.IsMatch("aab", "c*a*b"));
+            Assert.IsTrue(re.IsMatch("", ".*"));
+        }
+    }
+}
